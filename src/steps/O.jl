4bd426e39140df25bd230eebdcc8440eba845fac
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-01-30 12:43:09 +0100

    fire in case of negative profits

--- src/steps/O.jl
+++ src/steps/O.jl
@@ -3,15 +3,38 @@ function stepO!(m::Model)
     # println("O")
     s = m.s[m.t]
     for f = s.FCs
-        f.π = min(max(0, floor(Int, m.p.ρΠ * (f.pF * f.s - f.wF))), f.D)
-        f.D -= f.π
-        s.B.D += f.π
-        s.B.Π += f.π
+        f.π = floor(Int, m.p.ρΠ * (f.pF * f.s - f.wF))
+        if f.π > 0
+            f.π = min(f.π, f.D)
+            f.D -= f.π
+            s.B.D += f.π
+            s.B.Π += f.π
+        else
+            ws = sort(map(id -> state.Hs[id], f.employees), by=h -> h.σ, rev=true)
+            while f.π < 0 && length(ws) > 0
+                h = popfirst!(ws)
+                fire!(h)
+                filter!(i -> i != h.id, f.employees)
+                f.π += h.wF
+            end
+            f.π = 0
+        end
     end
     for f = s.FKs
-        f.π = min(max(0, floor(Int, m.p.ρΠ * (f.p * f.s - f.wF))), f.D)
-        f.D -= f.π
-        s.B.D += f.π
-        s.B.Π += f.π
+        f.π = floor(Int, m.p.ρΠ * (f.pF * f.s - f.wF))
+        if f.π > 0
+            f.π = min(f.π, f.D)
+            s.B.D += f.π
+            s.B.Π += f.π
+        else
+            ws = sort(map(id -> state.Hs[id], f.employees), by=h -> h.σ, rev=true)
+            while f.π < 0 && length(ws) > 0
+                h = popfirst!(ws)
+                fire!(h)
+                filter!(i -> i != h.id, f.employees)
+                f.π += h.wF
+            end
+            f.π = 0
+        end
     end
 end
