988b2641765f63dd0303eea6787ac8a9d445d120
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-01-31 22:56:53 +0100

    typo

--- src/steps/N.jl
+++ src/steps/N.jl
@@ -3,10 +3,10 @@ function stepN!(m::Model)
     s = m.s[m.t]
     for f = s.FCs
         for l = f.L
-            i = floor(Int, l.r * l.value)
-            Δv = ceil(Int, l.value / (m.p.NL - l.age))
+            i = floor(Int, l.r * l.principal)
+            Δv = ceil(Int, l.principal / (m.p.NL - l.age))
             if f.D < (i + Δv)
-                l.value += i
+                l.principal += i
                 l.age -= 1
                 l.NPL = true
                 s.B.L += i
@@ -15,19 +15,19 @@ function stepN!(m::Model)
                 s.B.D += (i + Δv)
                 f.iL -= i
                 s.B.iL += i
-                l.value -= Δv
+                l.principal -= Δv
                 l.NPL = false
                 s.B.L -= Δv
             end
         end
-        filter!(l -> l.value > 0, f.L)
+        filter!(l -> l.principal > 0, f.L)
     end
     for f = s.FKs
         for l = f.L
-            i = floor(Int, l.r * l.value)
-            Δv = ceil(Int, l.value / (m.p.NL - l.age))
+            i = floor(Int, l.r * l.principal)
+            Δv = ceil(Int, l.principal / (m.p.NL - l.age))
             if f.D < (i + Δv)
-                l.value += i
+                l.principal += i
                 l.age -= 1
                 l.NPL = true
                 s.B.L += i
@@ -36,11 +36,11 @@ function stepN!(m::Model)
                 s.B.D += (i + Δv)
                 f.iL -= i
                 s.B.iL += i
-                l.value -= Δv
+                l.principal -= Δv
                 l.NPL = false
                 s.B.L -= Δv
             end
         end
-        filter!(l -> l.value > 0, f.L)
+        filter!(l -> l.principal > 0, f.L)
     end
 end
