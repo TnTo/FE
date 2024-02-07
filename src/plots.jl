860ce781df2a209b1451c2583fe3aaa37638581e
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-02-07 16:23:08 +0100

    typo

--- src/plots.jl
+++ src/plots.jl
@@ -1,9 +1,9 @@
 using Plots
 
 function map_plot(m::Model, f, label::String)
-    plot(map(f, m.s), label=label, legend=:outerright, gridalpha=0.3, show=true)
+    display(plot(map(f, m.s), label=label, legend=:outerright, gridalpha=0.3, show=true))
 end
 
 function map_plot(m::Model, fs::Vector{Function}, labels::Matrix{String})
-    plot(map(f -> map(f, m.s), fs), label=labels, legend=:outerright, gridalpha=0.3, show=true)
+    display(plot(map(f -> map(f, m.s), fs), label=labels, legend=:outerright, gridalpha=0.3, show=true))
 end
--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,5 @@
 mapsum(f, itr) = mapreduce(f, +, itr, init=0)
+mapmean(f, itr) = mean(map(f, itr))
 
 yearly2monthly(r::Float)::Float = (1 + r)^(1 / 12) - 1
 y2m = yearly2monthly
