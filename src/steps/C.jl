ed9036cce637857b411a96d151756d0ab86e83d4
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-02-07 12:36:37 +0100

    fix rS sign

--- src/steps/C.jl
+++ src/steps/C.jl
@@ -4,7 +4,7 @@ function stepC!(m::Model)
     s = m.s[m.t]
     if m.s[m.t-1].B.L == 0
         if v(m, m.s[m.t-1].B) > 0
-            s.B.rS = s.G.rB + m.p.λ
+            s.B.rS = max(0, s.G.rB + m.p.λ)
             s.B.l_ = max(0, floor(Int, v(m, m.s[m.t-1].B) / m.p.Γ_ / (m.p.ν1 * (m.p.NFC + m.p.NFK))))
             s.B.rL = max(0, s.G.rB)
         else
@@ -13,7 +13,7 @@ function stepC!(m::Model)
             s.B.rL = max(0, s.G.rB + m.p.ν2)
         end
     else
-        s.B.rS = s.G.rB + m.p.λ * (Γ0 - m.p.Γ_)
+        s.B.rS = max(0, s.G.rB + m.p.λ * (Γ0 - m.p.Γ_))
         s.B.l_ = max(0, floor(Int, m.s[m.t-1].B.L * (Γ0 / m.p.Γ_ - 1) / (m.p.ν1 * (m.p.NFC + m.p.NFK))))
         s.B.rL = max(0, s.G.rB + m.p.ν2 * (m.p.Γ_ - Γ0))
     end
