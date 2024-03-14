c458cb1a09a27461aa6bf72785c5c7e9c62c9b15
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-03-14 22:32:53 +0100

    stricter bounds

--- scripts/calibrate.jl
+++ scripts/calibrate.jl
@@ -10,39 +10,41 @@ using BayesOpt
 
 config = ConfigParameters()
 config.random_seed = 8686
+config.n_iterations = 500
+config.n_iter_relearn = 25
 
 bounds = [
-    0.0 20.0; # σ0 
+    0.0 5.0; # σ0 
     0.0 1.0; # δ0 
-    0.0 20.0; # β0 
-    0.0 100.0; # e0 
+    0.0 5.0; # β0 
+    0.0 5.0; # e0 
     0.0 1.0; # e1 
-    1.0 10.0; # ρH 
-    0.0 100.0; # ay 
-    0.0 100.0; # av 
-    1.0 10.0; # ρC 
-    1.0 10.0; # ρK 
-    1.0 10.0; # ρF 
-    0.0 10.0; # Θ 
-    1.0 100.0; # k 
+    1.0 2.0; # ρH 
+    0.0 10.0; # ay 
+    0.0 10.0; # av 
+    1.0 2.0; # ρC 
+    1.0 2.0; # ρK 
+    1.0 2.0; # ρF 
+    0.0 5.0; # Θ 
+    1.0 10.0; # k 
     0.0 1.0; # ρΠ 
-    0.0 10.0; # ρQ 
-    0.0 10.0; # λ 
+    0.0 1.0; # ρQ 
+    0.0 1.0; # λ 
     0.0 1.0; # ν0 
     0.0 1.0; # ν1 
-    0.0 10.0; # ν2 
-    0.0 10.0; # ν3 
-    0.0 10.0; # ν4 
-    0.0 100.0; # τF 
-    0.0 100.0; # τT 
+    0.0 1.0; # ν2 
+    0.0 1.0; # ν3 
+    0.0 1.0; # ν4 
+    0.0 10.0; # τF 
+    0.0 10.0; # τT 
     0.0 1.0; # ϵ0 
-    0.0 100.0; # ϵ1 
-    0.0 100.0; # ζ 
-    0.0 10.0; # b0 
-    0.0 10.0; # b1 
-    0.0 10.0 # b2 
+    0.0 5.0; # ϵ1 
+    0.0 5.0; # ζ 
+    0.0 5.0; # b0 
+    0.0 5.0; # b1 
+    0.0 5.0 # b2 
 ]
 
 optimizer, optimum = bayes_optimization(DAS.run_or_load, bounds[:, 1], bounds[:, 2], config)
 
-DrWatson.tagsave("optimization", Dict(:optimizer => optimizer, :optimum => optimum))
+DrWatson.tagsave("optimization.jld2", Dict(:optimizer => optimizer, :optimum => optimum), path="data")
