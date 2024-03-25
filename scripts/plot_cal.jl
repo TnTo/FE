389a9c2093fffd18323e04702e60cbb9a8ffea81
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-03-25 12:54:01 +0100

    fix uneven pars

--- scripts/plot_cal.jl
+++ scripts/plot_cal.jl
@@ -12,17 +12,13 @@ using StatsPlots
 
 df = collect_results(datadir("sims"), white_list=["config", "score"])
 df[!, "normalized_score"] = df[!, "score"] / (300 * 21 * 4)
-transform!(
-    df,
-    (
-        :config => ByRow(DrWatson.dict2ntuple) => AsTable
-    )
-)
 
+df = hcat(df, DataFrame(Tables.dictrowtable(DrWatson.dict2ntuple.(df[!, :config]))))
 df2 = select(df, Not(:score, :path, :config))
 
 for p = filter(c -> c != "normalized_score", names(df2))
-    npar = mean_and_std(df2[!, p], weights(df2[!, :normalized_score]))
+    idx = .!(ismissing.(df2[!, p]))
+    npar = mean_and_std(disallowmissing(df2[idx, p]), weights(df2[idx, :normalized_score]))
     scatter(df2[!, p], df2[!, :normalized_score], label=false, title=p)
     plot!(Normal(npar...), label=false)
     display(hline!([0.10], label=false))
