9ab535fddbc123dd2bb9c361f1914ec61eb38400
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-03-25 11:38:37 +0100

    new profit eq

--- src/steps/O.jl
+++ src/steps/O.jl
@@ -3,38 +3,44 @@ function stepO!(m::Model)
     @debug "O"
     s = m.s[m.t]
     for f = s.FCs
-        f.π = floor(Int, m.p.ρΠ * (f.pF * f.s - f.wF))
-        if f.π > 0
-            f.π = min(f.π, f.D)
+        Δ = f.pF * f.s - f.wF
+        if Δ < 0
+            ws = sort(map(id -> s.Hs[id], f.employees), by=h -> h.σ, rev=true)
+            while Δ < 0 && length(ws) > 0
+                h = popfirst!(ws)
+                fire!(h)
+                filter!(i -> i != h.id, f.employees)
+                Δ += h.wF
+            end
+        end
+        D_ = ρF * (f.wF + f.i)
+        if D_ < f.D
+            f.π = f.D - D_
             f.D -= f.π
             s.B.D += f.π
             s.B.Π += f.π
         else
-            ws = sort(map(id -> s.Hs[id], f.employees), by=h -> h.σ, rev=true)
-            while f.π < 0 && length(ws) > 0
-                h = popfirst!(ws)
-                fire!(h)
-                filter!(i -> i != h.id, f.employees)
-                f.π += h.wF
-            end
             f.π = 0
         end
     end
     for f = s.FKs
-        f.π = floor(Int, m.p.ρΠ * (f.p * f.s - f.wF))
-        if f.π > 0
-            f.π = min(f.π, f.D)
+        Δ = f.p * f.s - f.wF
+        if Δ < 0
+            ws = sort(map(id -> s.Hs[id], f.employees), by=h -> h.σ, rev=true)
+            while Δ < 0 && length(ws) > 0
+                h = popfirst!(ws)
+                fire!(h)
+                filter!(i -> i != h.id, f.employees)
+                Δ += h.wF
+            end
+        end
+        D_ = ρF * f.wF
+        if D_ < f.D
+            f.π = f.D - D_
             f.D -= f.π
             s.B.D += f.π
             s.B.Π += f.π
         else
-            ws = sort(map(id -> s.Hs[id], f.employees), by=h -> h.σ, rev=true)
-            while f.π < 0 && length(ws) > 0
-                h = popfirst!(ws)
-                fire!(h)
-                filter!(i -> i != h.id, f.employees)
-                f.π += h.wF
-            end
             f.π = 0
         end
     end
