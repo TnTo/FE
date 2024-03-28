bf18d81e031f55d231ac6190bd2f6f800f4ecc54
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2024-03-28 12:17:57 +0100

    better diagnostic tools

--- scripts/explore_best.jl
+++ scripts/explore_best.jl
@@ -30,4 +30,87 @@ for i = 1:4
     scores[i] = DAS.evaluate_model(ms[i])
 end
 
-ms
+for m = ms
+    DAS.display_matrices(m, m.t)
+
+    DAS.map_plot(m,
+        [
+            s -> DAS.mapsum(h -> h.rc_, s.Hs),
+            s -> DAS.mapsum(h -> h.rc, s.Hs),
+            s -> s.G.rC,
+            s -> DAS.mapsum(f -> f.c_, s.FCs),
+            s -> DAS.mapsum(f -> f.c, s.FCs),
+            s -> DAS.mapsum(f -> DAS.b(m, f), s.FCs),
+            s -> DAS.mapsum(f -> length(f.employees), s.FCs)
+        ],
+        ["H rc_" "H rc" "G rc" "F c_" "F c" "F b" "emp"]
+    )
+
+    DAS.map_plot(m,
+        [
+            s -> DAS.mapsum(f -> length(f.inv), s.FKs),
+            s -> DAS.mapsum(f -> f.Δb_, s.FCs),
+            s -> DAS.mapsum(f -> f.Δb, s.FCs),
+            s -> DAS.mapsum(f -> f.Δb_, s.FKs),
+            s -> DAS.mapsum(f -> f.k_ * f.β * m.p.k, s.FKs),
+            s -> DAS.mapsum(f -> f.k * f.β * m.p.k, s.FKs),
+            s -> DAS.mapsum(f -> DAS.b(m, f), s.FKs),
+            s -> DAS.mapsum(f -> length(f.employees), s.FKs),
+            s -> DAS.mapsum(f -> f.s, s.FKs),
+        ],
+        ["inv" "Δb_ FC" "Δb FC" "Δb_ FK" "k_" "k" "b FK" "empl FK" "s"]
+    )
+
+    DAS.map_plot(m,
+        [
+            s -> count(h -> h.employer !== nothing, s.Hs),
+            s -> count(h -> h.employer === nothing, s.Hs)
+        ],
+        ["empl" "unempl"]
+    )
+
+    DAS.map_plot(m, [s -> s.B.rS, s -> s.G.rB], ["rS" "rB"])
+
+    DAS.map_plot(m,
+        [
+            s -> DAS.mapsum(f -> length(f.inv), s.FKs),
+            s -> DAS.mapsum(f -> length(f.employees), s.FKs),
+            s -> DAS.mapsum(f -> f.s, s.FKs),
+            s -> DAS.mapsum(f -> f.k, s.FKs),
+            s -> DAS.mapsum(f -> DAS.b(m, f), s.FKs),
+            s -> DAS.mapsum(f -> length(f.K), s.FKs),
+            s -> DAS.mapsum(f -> f.k_, s.FKs),
+        ],
+        ["inv" "empl FK" "s FK" "k FK" "b FK" "K FK" "k_ FK"]
+    )
+
+    DAS.map_plot(m,
+        [
+            s -> DAS.mapsum(f -> length(f.Q), s.FKs),
+            s -> DAS.mapsum(f -> f.q_, s.FKs),
+        ],
+        ["Q" "q_"]
+    )
+
+    DAS.map_plot(m,
+        [
+            s -> DAS.mapmean(f -> f.p, s.FKs),
+            s -> DAS.mapmean(f -> f.pF, s.FCs),
+            s -> DAS.mapmean(h -> h.wF, filter(h -> h.employer !== nothing, s.Hs)),
+            s -> 0,
+            s -> s.stats.p,
+            s -> DAS.mapmean(h -> h.D, s.Hs)
+        ],
+        ["pK" "pC" "w" "0" "<p>" "DH"]
+    )
+
+    DAS.map_plot(m,
+        [
+            s -> DAS.mapmean(f -> f.μ, s.FKs),
+            s -> DAS.mapmean(f -> f.μ, s.FCs),
+        ],
+        ["μK" "μC"]
+    )
+
+    scatter(map(h -> h.σ, m.s[end].Hs), map(h -> h.wF, m.s[end].Hs))
+end
--- scripts/plot_cal.jl
+++ scripts/plot_cal.jl
@@ -14,8 +14,8 @@ df = collect_results(datadir("sims"), white_list=["config", "score"])
 df[!, "normalized_score"] = df[!, "score"] / (300 * 21 * 4)
 
 df = hcat(df, DataFrame(Tables.dictrowtable(DrWatson.dict2ntuple.(df[!, :config]))))
-df2 = select(df, Not(:score, :path, :config))
-df2 = df2[df2.normalized_score.>=0.1, :]
+df = select(df, Not(:score, :path, :config))
+df2 = df[df.normalized_score.>=0.15, :]
 
 D = 1
 
@@ -24,6 +24,9 @@ for p = filter(c -> c != "normalized_score", names(df2))
     npar = mean_and_std(disallowmissing(df2[idx, p]), weights(df2[idx, :normalized_score] .* D))
     scatter(df2[!, p], df2[!, :normalized_score], label=false, title=p)
     plot!(Normal(npar...), label=false)
+    idx = .!(ismissing.(df[!, p]))
+    npar = mean_and_std(disallowmissing(df[idx, p]), weights(df[idx, :normalized_score] .* D))
+    plot!(Normal(npar...), label=false, linestyle=:dash)
     display(hline!([0.10], label=false))
 end
 
